2779fc6a318dd3916919d87b7f8a17dbb5394d18
lucciano01 <lucciano01@hotmail.com>
2025-08-26 21:13:21 -0300

    metodos de somar e multiplicar

---
+++ exemplo_git/calculadora/src/main/java/br/edu/catolica/calculo/Calcular.java
@@ -0,0 +1,12 @@
+package br.edu.catolica.calculo;
+
+public class Calcular {
+
+    public int somar(int a, int b){
+        return a+b;
+    }
+
+    public int multiplicat(int a, int b){
+        return a*b;
+    }
+}
